3d7df5057bff673c4f17eaf3c3a8fdd1c31ca0c2
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-18 10:53:47 +0300

    added db_id and collection_id consts

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,7 +1,12 @@
-import { Client } from 'appwrite';
+import { Client, Databases } from 'appwrite';
+
+export const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
+export const COLLECTION_ID_MESSAGES = import.meta.env.VITE_COLLECTION_ID_MESSAGES;
 
 const client = new Client();
 client
     .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
     .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 export default client;
+export const databases = new Databases(client);
