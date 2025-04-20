44857b6e251814888f6e22ab9dbddecee5498242
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-20 22:27:36 +0300

    login changes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,57 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../utils/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const { user } = useAuth();
+  const { user, handleUserLogin } = useAuth();
   const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
   useEffect(() => {
     if (user) {
       navigate("/");
     }
   }, []);
-  return <p>Login</p>;
+
+  const handleInputChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setCredentials({...credentials, [name]:value});
+    console.log(credentials);
+  };
+  return (
+    <div>
+      <div>
+        <form>
+          <div>
+            <label htmlFor="">Email:</label>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Enter your email"
+              value={credentials.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="">Password:</label>
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Enter your passwordl"
+              value={credentials.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <input type="submit" value="Login" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
