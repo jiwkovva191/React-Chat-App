32bad6c53f3bd1e6cfa281427b036f25cee0626e
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-22 13:24:28 +0300

    login page styling

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -23,10 +23,10 @@ export default function Login() {
     console.log(credentials);
   };
   return (
-    <div>
-      <div>
-        <form onSubmit={(e)=>{handleUserLogin(e,credentials)}}>
-          <div>
+    <div className="min-h-screen flex items-center justify-center bg-[#2e2e2ed6]">
+      <div className="flex flex-col gap-[20px] bg-[#2E2E2E] p-[30px] my-[20px] rounded-2xl">
+        <form className="flex flex-col gap-[10px] text-white font-semibold text-[18px]" onSubmit={(e)=>{handleUserLogin(e,credentials)}}>
+          <div className="flex flex-col gap-[10px]">
             <label htmlFor="">Email:</label>
             <input
               type="email"
@@ -35,9 +35,13 @@ export default function Login() {
               placeholder="Enter your email"
               value={credentials.email}
               onChange={handleInputChange}
+              autoComplete="off"
+              readOnly
+              onFocus={(e) => e.target.removeAttribute('readOnly')}
+              className="bg-transparent border-b text-white"
             />
           </div>
-          <div>
+          <div className="flex flex-col gap-[10px]">
             <label htmlFor="">Password:</label>
             <input
               type="password"
@@ -46,13 +50,14 @@ export default function Login() {
               placeholder="Enter your password"
               value={credentials.password}
               onChange={handleInputChange}
+              className="bg-transparent border-b text-white"
             />
           </div>
           <div>
-            <input type="submit" value="Login" />
+            <input className="bg-[#10B981] px-[10px] py-[5px] rounded-md font-bold text-white cursor-pointer" type="submit" value="Login" />
           </div>
         </form>
-        <p>Don't have an account? Register <Link to ="/register">here</Link></p>
+        <p className="text-white font-semibold">Don't have an account? Register <Link className="text-[#10B981]" to ="/register">here</Link>.</p>
       </div>
     </div>
   );
